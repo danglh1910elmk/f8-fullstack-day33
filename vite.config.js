438d786f868dfb0e536f3868e36876c7d40892a9
danglh1910elmk <danglh1910elmk@gmail.com>
2025-11-01 19:45:38 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/f8-fullstack-day33/",
     // cấu hình dùng đường dẫn tuyệt đối
     resolve: {
         alias: {
